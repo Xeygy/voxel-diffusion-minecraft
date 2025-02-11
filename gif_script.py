ae71dcb47b6c2fc2f9e6f361de5563c6bbc07740
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2025-02-11 07:59:31 -0800

    add cli and resize options for gif_script

--- gif_script.py
+++ gif_script.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import os
+import argparse
 
 def to_gif(paths, name='output'):
     im_list = []
@@ -11,18 +12,26 @@ def to_gif(paths, name='output'):
                     append_images=im_list[1:] + [im_list[-1]] * 10, 
                     loop=0, duration=500)
 
-def to_gif_imlist(im_list, name='output'):
+def to_gif_imlist(im_list, name='output', frame_duration=500, resize_width=None):
+    if resize_width is not None:
+        width, height = im_list[0].size
+        new_width  = resize_width
+        new_height = new_width * height / width 
+
+        for i, im in enumerate(im_list):
+            im.thumbnail((new_width, new_height), Image.LANCZOS)
+            im_list[i] = im
     im_list[0].save(f'{name}.gif', 
                     save_all=True, 
                     append_images=im_list[1:] + [im_list[-1]] * 10, 
-                    loop=0, duration=500)
+                    loop=0, duration=frame_duration)
 
 def giffed_single(dir_path):
     paths = [dir_path + f for f in os.listdir(dir_path) if f.endswith('.png')]
     paths.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]))
     to_gif(paths, "monocube")
     
-def giffed_multi(dir_path):
+def giffed_multi(dir_path, gif_name='multicube'):
     '''
     expects pngs of the form
     xxxx_n_s.png
@@ -45,7 +54,7 @@ def giffed_multi(dir_path):
     for key in keys:
         im_list = dict_paths[key]
         im_list.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]))
-        
+
         n = len(im_list)
         sqrtn = int(n**0.5)
 
@@ -57,9 +66,14 @@ def giffed_multi(dir_path):
             im = Image.open(im_path)
             grid.paste(im, (width * (i % sqrtn), height * (i // sqrtn)))
         ims.append(grid)
-    to_gif_imlist(ims, "multicube")
+    to_gif_imlist(ims, gif_name, 200)
     
 if __name__ == '__main__':
-    giffed_multi('3d_cube20250210_220958')
+    # add arg for directory
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir', type=str, help='directory containing pngs')
+    parser.add_argument('name', type=str, help='name of gif', )
+    args = parser.parse_args()
+    giffed_multi(args.dir, args.name)
     
     
